f3213d55617d28a2822b1a14c8142c08ee030283
Vincent Cannataro <vincent.cannataro@yale.edu>
2018-11-16 10:02:14 -0500

    added working script and testing data sync

---
+++ ML_cesR_main_analysis_script.R
@@ -0,0 +1,14 @@
+
+#### Load in mutation data ----
+
+NCI_data <- read.delim(file = "~/Box Sync/ML_cancereffectsizeR_data/LUAD/TCGA.LUAD.mutect.0458c57f-316c-4a7c-9294-ccd11c97c2f9.DR-10.0.somatic.maf",stringsAsFactors = F,skip = 5,header = T)
+
+
+
+
+
+
+
+
+
+
