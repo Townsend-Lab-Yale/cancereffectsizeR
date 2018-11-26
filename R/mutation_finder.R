c73f589e4ca7c234eca7a18a3082b3f6e6f5aab9
Vincent Cannataro <vincent.cannataro@yale.edu>
2018-11-26 11:45:08 -0500

    fixed comments

--- R/mutation_finder.R
+++ R/mutation_finder.R
@@ -1,28 +1,11 @@
 # function to find trinucleotides for when a variant is up against an exon edge.
 
-# OR2L13 M1T
-
-# > MAF_input[i,]
-# Unique_patient_identifier Chromosome Start_Position
-# 128852              TCGA-MP-A4TA          1      248262679
-# Reference_Allele Tumor_allele Gene_name strand triseq
-# 128852                T            C    OR2L13      +    ATG
-# unique_variant_ID is_coding Tumor_allele_correct_strand trinuc_dcS
-# 128852     1 248262679 C      TRUE                           C    A[T>C]G
-# nuc_variant coding_variant nuc_position codon_pos
-# 128852         T2C            M1T            2         2
-# amino_acid_context unique_variant_ID_AA coding_variant_AA_mut
-# 128852            TCATGGA                  M1T                   Thr
-
-# MAF_input_row <- MAF_input[118273,]
-# RefCDS_instance <- RefCDS[["AL161645.2"]]
 
 AA_translations$Nucs <- as.character(AA_translations$Nucs)
 
 AA_translations$AA_short <- as.character(AA_translations$AA_short)
 
 
-# any(abs(RefCDS[["OR2L13"]]$intervals_cds - 248262679) <= 3)
 
 mutation_finder <- function(RefCDS_instance,MAF_input_row){
 
@@ -97,20 +80,3 @@ mutation_finder <- function(RefCDS_instance,MAF_input_row){
 return(trinucs_for_rate)
 }
 
-mutation_finder(RefCDS_instance = RefCDS[["OR2L13"]],MAF_input_row <- MAF_input[118273,])
-
-
-#
-# substr(as.character(RefCDS[["NFE2L2"]]$seq_cds),start = 593,stop = 595)
-#
-# RefCDS[["NFE2L2"]]$intervals_cds
-#
-# chr2cds(pos = 178098733,cds_int = RefCDS[["NFE2L2"]]$intervals_cds,strand = -1)
-#
-# chr2cds(pos = 178097120,cds_int = RefCDS[["NFE2L2"]]$intervals_cds,strand = -1)
-#
-# substr(as.character(RefCDS[["NFE2L2"]]$seq_cds),start = 593,stop = 595)
-# substr(as.character(RefCDS[["NFE2L2"]]$seq_cds1down),start = 592,stop = 594)
-#
-#
-#
