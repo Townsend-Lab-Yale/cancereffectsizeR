aa578563a685047e59fafd56e2cab1fd8588a426
Vincent Cannataro <vincent.cannataro@yale.edu>
2019-07-03 14:25:59 -0400

    split functions into sep .R files

--- R/classes.R
+++ R/classes.R
@@ -1,4 +1,4 @@
-setClass("CESProgressions", representation(order = "numeric", progression_by_tumor = "environment", 
+setClass("CESProgressions", representation(order = "numeric", progression_by_tumor = "environment",
                                             tumors_by_progression = "environment"))
 setValidity("CESProgressions",
   function(object) {
@@ -27,7 +27,7 @@ setValidity("CESProgressions",
         if (length(invalid_progressions) > 0) {
           if (length(invalid_progressions) > 10) {
             num_additional = length(invalid_progressions) - 10
-            invalid_progressions = paste0(paste(invalid_progressions[1:10], collapse=", "), " (and ", num_additional, " more)") 
+            invalid_progressions = paste0(paste(invalid_progressions[1:10], collapse=", "), " (and ", num_additional, " more)")
           } else {
             invalid_progressions = paste(invalid_progressions, collapse=", ")
           }
@@ -43,36 +43,9 @@ setValidity("CESProgressions",
   }
 )
 
-CESProgressions = function(samples, sample_progressions, ordered_progression_labels) {
-  if(length(samples) != length(sample_progressions)) {
-    stop("Samples and progressions vectors are not equal-length.")
-  }
-  progression_order = 1:length(ordered_progression_labels)
-  names(progression_order) = ordered_progression_labels
-  progression_by_tumor = new.env()
-  tumors_by_progression = new.env()
-  for (i in 1:length(samples)) {
-    tumor = as.character(samples[i])
-    progression_label = sample_progressions[i]
-    progression_number = progression_order[[progression_label]]
-    if (exists(tumor, progression_by_tumor) && progression_by_tumor[[tumor]] != progression_number) {
-      stop(paste0("Tumor ", tumor, " has multiple progression states in the input MAF."))
-    } else {
-      progression_by_tumor[[tumor]] = progression_number
-    }
-    if (exists(as.character(progression_number), tumors_by_progression) && ! tumor %in% tumors_by_progression[[as.character(progression_number)]]) {
-      tumors_by_progression[[as.character(progression_number)]] = c(tumor, tumors_by_progression[[as.character(progression_number)]])
-    } else if (! exists(as.character(progression_number), tumors_by_progression)) {
-      tumors_by_progression[[as.character(progression_number)]] = tumor
-    }
-  }
 
-  progressions = new("CESProgressions", order = progression_order, progression_by_tumor = progression_by_tumor,
-                          tumors_by_progression = tumors_by_progression) 
-  return(progressions)
-}
 
-setGeneric("get_progression_number", function(a, b) standardGeneric("get_progression_number")) 
+setGeneric("get_progression_number", function(a, b) standardGeneric("get_progression_number"))
 setMethod("get_progression_number", signature("CESProgressions", "character"), function(a, b) {
   progressions = a; tumor = b
   return(sapply(tumor, function(x) { progressions@progression_by_tumor[[x]]}))
@@ -85,7 +58,7 @@ setMethod("get_progression_name", signature("CESProgressions", "character"), fun
 })
 
 setGeneric("get_progression_tumors", function(a, b) standardGeneric("get_progression_tumors"))
-setMethod("get_progression_tumors", signature("CESProgressions", "numeric"), 
+setMethod("get_progression_tumors", signature("CESProgressions", "numeric"),
   function(a, b) {
     progressions = a; progression_number = b
     return(progressions@tumors_by_progression[[as.character(b)]])
@@ -115,17 +88,15 @@ setValidity("MAFdf",
 )
 
 
-MAFdf = function(df, ...) {
-  new("MAFdf", df, ...)
-}
 
-setClass("CESAnalysis", representation(main.maf = "MAFdf", annotated.snv.maf = "MAFdf", trinucleotide_mutation_weights = "list", 
+
+setClass("CESAnalysis", representation(main.maf = "MAFdf", annotated.snv.maf = "MAFdf", trinucleotide_mutation_weights = "list",
           progressions = "CESProgressions", mutrates_list = "list", dndscv_out_list = "list",
           relative_substitution_rates = "table", refcds_data = "array", excluded = "environment", selection_results = "list"))
 
 ## To-do: Add generic methods for editing slots that enfore these validity checks
 ## Currently, validity is only checked on object creation, so downstream functions can mess up the object
-setValidity("CESAnalysis", 
+setValidity("CESAnalysis",
     function(object) {
       ## Can add more validation later
       problems = character()
