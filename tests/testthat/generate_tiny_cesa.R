fdaf0e3330d26c910b5f9fe31bec08bc6c790a0c
Jeff Mandell <jeff.mandell@yale.edu>
2020-01-13 11:41:06 -0500

    Reorganized test data generation scripts

--- tests/testthat/generate_tiny_cesa.R
+++
@@ -1,3 +0,0 @@
-setwd(system.file("tests/testthat/testdata", package = "cancereffectsizeR"))
-tiny_ak = load_maf(cesa = CESAnalysis(), maf = "tiny.hg19.maf.txt", sample_col = "sample_id", tumor_allele_col = "Tumor_Seq_Allele2")
-saveRDS(tiny_ak, "tiny_hg19_maf_loaded.rds")
